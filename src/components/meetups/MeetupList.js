bf0c51d4962343092330ae33fa54add3991e3fd4
francis omondi <francisomondi17@gmail.com>
2022-06-17 15:48:58 +0300

    .

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MeetupList = () => {
+  return (
+    <div>
+      <h2>this will contain all the meetup list</h2>
+    </div>
+  );
+};
+
+export default MeetupList;
