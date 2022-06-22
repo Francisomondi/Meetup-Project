6219d0e5ec3222afd6999a89019af2ceb95e0509
francis omondi <francisomondi17@gmail.com>
2022-06-22 17:43:43 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetups />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/newmeetup" element={<NewMeetup />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
