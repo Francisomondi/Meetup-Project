b3c6a9a8b5cc41119546034c2feef0298892ab2e
francis omondi <francisomondi17@gmail.com>
2022-06-23 12:10:05 +0300

    getting user inpur using the useRef hook

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,29 +1,51 @@
-import React from "react";
+import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
 const NewMeetupForm = () => {
+  const titleInputref = useRef();
+  const imageInputref = useRef();
+  const addressInputref = useRef();
+  const descriptionInputref = useRef();
+
   const handleSubmit = event => {
     event.preventDefault();
+
+    const enteredTitle = titleInputref.current.value;
+    const enteredImage = imageInputref.current.value;
+    const enteredAddress = addressInputref.current.value;
+    const enteredDescription = descriptionInputref.current.value;
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription
+    };
+    console.log(meetupData);
   };
   return (
     <Card>
       <form className={classes.form} onSubmit={handleSubmit}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputref} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputref} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputref} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputref}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button> Add Meetup</button>
