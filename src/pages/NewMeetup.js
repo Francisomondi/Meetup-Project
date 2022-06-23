bfa5ad2823db4b58bf9bfb4f71ae06fe5ca040da
francis omondi <francisomondi17@gmail.com>
2022-06-23 14:58:39 +0300

    saving data input to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,22 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetup = () => {
+  const addMeetupHandler = meetupData => {
+    fetch(
+      "https://react-meetup-project-87fa1-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  };
   return (
     <section>
       <h1>Add New MeetUp</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
